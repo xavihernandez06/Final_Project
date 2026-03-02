1b4d5a6af2b208c8dcdd1fec07a04e5aed5eacb3
Andrei <xavihernandez06@yahoo.com>
2026-03-02 17:27:16 +0200

    Finalized Logout and pokeball

--- pokemon_game/pokemon_game/urls.py
+++ pokemon_game/pokemon_game/urls.py
@@ -4,6 +4,8 @@ from django.urls import path, include
 from pokedex.models import StatusEffect, TypeEffectiveness, PokemonType, Attack, Pokemon
 from pokedex.views import home, pokedex_view, create_pokemon
 
+from django.contrib.auth.views import LogoutView
+
 admin.site.register(StatusEffect)
 admin.site.register(TypeEffectiveness)
 admin.site.register(PokemonType)
@@ -15,5 +17,6 @@ urlpatterns = [
     path('pokedex/', pokedex_view, name='pokedex'),
     path('', home, name='home'),
     path("create/", create_pokemon, name="create_pokemon"),
+    path('accounts/logout/', LogoutView.as_view(next_page='home'), name='logout'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
